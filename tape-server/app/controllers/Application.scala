3b3c005fff0f3f760769b0da03a7d182f8a482a6
Tony Marjakangas <tony@marza.se>
2014-02-13 18:36:07 +0100

    Store REST method

--- tape-server/app/controllers/Application.scala
+++ tape-server/app/controllers/Application.scala
@@ -2,11 +2,22 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import scala.concurrent._
+
+import ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def store = Action(parse.raw) { request =>
+    future {
+      println(request.body.asBytes())
+    }
+
+    Ok
   }
 
 }
