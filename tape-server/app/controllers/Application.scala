0a9a13130745dd934f4fe9bf07626bbd8bd3c49b
Tony Marjakangas <tony@marza.se>
2014-02-15 15:37:39 +0100

    WS server

--- tape-server/app/controllers/Application.scala
+++ tape-server/app/controllers/Application.scala
@@ -5,6 +5,7 @@ import play.api.mvc._
 import scala.concurrent._
 
 import ExecutionContext.Implicits.global
+import play.api.libs.iteratee.{Enumerator, Iteratee}
 
 object Application extends Controller {
 
@@ -18,6 +19,13 @@ object Application extends Controller {
     }
 
     Ok
+  }
+
+  def ws = WebSocket.using[String] { request =>
+    val in = Iteratee.consume[String]()
+    val out = Enumerator("OK").andThen(Enumerator.eof)
+
+    (in, out)
   }
 
 }
